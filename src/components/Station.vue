7e0f7658affc88cc5ea962286bd76bcbe287f862
Rusty Eddy <rusty@rustyeddy.com>
2022-02-15 16:22:38 -0800

    got it working with ugly formatting

---
+++ src/components/Station.vue
@@ -0,0 +1,15 @@
+<script setup>
+  const props = defineProps([
+    'station',
+    ])
+</script>
+
+<template>
+  <div class="col">
+    <h2>Station {{ station.id }}</h2>
+    <ul class="list-group">
+      <li class="list-group-item" v-for="(val, sens) in station.sensors">{{ sens }} : {{ val }}</li>
+    </ul>
+  </div>
+
+</template>
--- src/components/Stations.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="greetings">
-    <h1 class="green">Stations HQ</h1>
-  </div>
-</template>

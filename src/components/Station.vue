022f885dc503d26100ab2b0fed9b6a61802bbf37
Rusty Eddy <rusty@rustyeddy.com>
2022-02-15 18:04:11 -0800

    added pinia the vuex successor

--- src/components/Station.vue
+++ src/components/Station.vue
@@ -1,14 +1,14 @@
 <script setup>
   const props = defineProps([
     'station',
-    ])
+  ])
 </script>
 
 <template>
   <div class="col">
     <h2>Station {{ station.id }}</h2>
     <ul class="list-group">
-      <li class="list-group-item" v-for="(val, sens) in station.sensors">{{ sens }} : {{ val }}</li>
+      <li class="list-group-item" v-for="(val, sens) in station.sensors" :key="val">{{ sens }} : {{ val }}</li>
     </ul>
   </div>
 
--- src/views/StationsView.vue
+++ src/views/StationsView.vue
@@ -1,36 +1,16 @@
 <script setup>
   import { reactive } from 'vue'
   import Station from '@/components/Station.vue'
+  import { useStationStore } from '@/stores/stations'
 
-  const state = reactive({
-    stations: [
-      {
-        id: "10.11.1.33", 
-        sensors: {
-          tempf: 78.8,
-          humidity: 12.2,
-          moisture: 3.9,
-          luminence: 88.5,
-        }
-      },
-      {
-        id: "10.11.2.11",
-        sensors: {
-          tempf: 72.9,
-          humidity: 19.1,
-          moisture: 5.19,
-          luminence: 82.15,
+  const stations = useStationStore()
 
-        }
-      }
-    ]
-  })
 </script>
 
 <template>
   <div class="stations container">
     <div class="row m-4 p-4">
-      <Station v-for="station in state.stations" :station="station" />
+      <Station v-for="station in stations.getStations" :station="station" />
     </div>
   </div>
 </template>
